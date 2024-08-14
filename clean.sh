8ffd7a1ddabebe9d0ea47c548ca13d2b90cecb01
Johannes Hostert <jhostert@ethz.ch>
2024-08-14 16:10:05 +0200

    reset including canary

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd output
 rm -rf ./*
-git reset --hard $(git rev-list --max-parents=0 --abbrev-commit HEAD)
+git reset --hard ef8baf51c4896de7c65d18b5d0674d574429e26a #$(git rev-list --max-parents=0 --abbrev-commit HEAD)
 git push -f
